25f2382cb559b3bd21ccdbcce754252fcfca151d
Alison Rose <144371851+xxalisonrose@users.noreply.github.com>
2024-02-02 15:48:56 +0000

    ttt1and2

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # DGMD-E-28-Assignment-2
 DGMD E-28 Developing Single Page Web Applications Assignment 2: Tic Tac Toe
+
+Part 1.1: ttt1.html
+
+Part 1.2: ttt2.html
---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+const NUM_SQUARES = 9;
+for (i=0; i<NUM_SQUARES; i++)
+{
+    id = "sq"+i;
+    document.write( "<div class='square' id='" + id + "'></div>" );
+}
+
+
